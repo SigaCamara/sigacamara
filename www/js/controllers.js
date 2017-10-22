2b0d760d7065f4adc7e2a004f14d9b1d1d43ac5e
GouveaHeitor <hgouvea@protonmail.com>
2017-10-22 13:49:19 -0200

    acredito que esse seja o ultimo commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -204,10 +204,10 @@ angular.module('starter.controllers', [])
 
     $.getScript("https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=visualization,geometry&key=AIzaSyDiSbeJ5PvIWg9kcBrXbx622gMl-mlZ5-o")
       .done(function(script, textStatus) {
-        var myLatlng = new google.maps.LatLng(-22.909883022, -47.062581047);
+        var myLatlng = new google.maps.LatLng(-22.895226371608913, -47.03026079999995);
 
         var map = new google.maps.Map(document.getElementById("map-canvas"), {
-          zoom: 8,
+          zoom: 12,
           center: myLatlng,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           styles: styles
@@ -222,6 +222,24 @@ angular.module('starter.controllers', [])
           data: data
         });
 
+        var gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
+
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
         heatmap.set('radius', heatmap.get('radius') ? null : 50);
 
         heatmap.setMap(map);
