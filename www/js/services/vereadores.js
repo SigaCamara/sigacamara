adfc84cfbeb26022cbb3a688e44d73ba2c98f7e6
GouveaHeitor <hgouvea@protonmail.com>
2017-10-22 02:23:26 -0200

    ajustando a mini-bio dos vereadores

--- www/js/services/vereadores.js
+++ www/js/services/vereadores.js
@@ -4,15 +4,18 @@ moduleServices.factory('Vereadores', function($http, $q, DB, Util, URL, MockHelp
     all: function() {
       // return promise with vereadores data
       var promise = $http.get(URL.endpoint("parlamentares")).then(function (response) {
-       
+
         var myFollowedVereadores = DB.load("vereador_follow");
 
         // flag nos vereadores que estou seguindo
         for(var i in response.data){
           var vereador = response.data[i];
           vereador.follow = (Util.getIndexOfItem(vereador, myFollowedVereadores) !== false);
+
+          vereador.biografia = vereador.biografia.replace(/<\/?\w+[^>]*\/?>/g);
+          vereador.biografia = vereador.biografia.replace(/undefined/i, "");
         }
-      
+
         return response.data;
       });
 
@@ -29,7 +32,7 @@ moduleServices.factory('Vereadores', function($http, $q, DB, Util, URL, MockHelp
     addFollow: function(vereador){
       return $q(function(resolve, reject) {
         setTimeout(function(){
-        
+
           myFollowedVereadores = DB.load("vereador_follow");
           if(!myFollowedVereadores){
             myFollowedVereadores = [];
@@ -42,7 +45,7 @@ moduleServices.factory('Vereadores', function($http, $q, DB, Util, URL, MockHelp
           } else {
             console.error("Tentativa de adicionar item que ja existe na lista de Follow vereadores");
           }
-            
+
         }, MockHelper.timeout());
       });
     },
@@ -54,7 +57,7 @@ moduleServices.factory('Vereadores', function($http, $q, DB, Util, URL, MockHelp
           if(indice !== false){
             myFollowedVereadores.splice(indice, 1);
           }
-          
+
           DB.save('vereador_follow', myFollowedVereadores);
           resolve(true);
         }, MockHelper.timeout());
